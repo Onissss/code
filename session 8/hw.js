80c9d99b5bf3bfba04f380786318130bbaecdd3b
Onissss <sonbui11113@gmail.com>
2021-11-03 20:27:27 +0700

    hw

--- session 8/hw.js
+++ session 8/hw.js
@@ -1,23 +1,58 @@
 // SS7
 // Ex1
-let phone = {
-    phoneName: 'iPhone 13 Pro Max',
-    brand: [
-        {
-            brandName: 'Apple'
-        },
-        {
-            foundedYear: 1976
-        }
+// let phone = {
+//     phoneName: 'iPhone 13 Pro Max',
+//     brand: [
+//         {
+//             brandName: 'Apple'
+//         },
+//         {
+//             foundedYear: 1976
+//         }
 
-    ],
-    price: 1099 + '$',
-    country: 'Vietnam,USA,China',
-    phoneAvailable: 5000,
-}
-console.log(phone);
+//     ],
+//     price: 1099 + '$',
+//     country: 'Vietnam,USA,China',
+//     phoneAvailable: 5000,
+// }
+// console.log(phone);
+
 // Ex2
-phone.releaseYear = 2021
-phone.price = 1099 + (1099 * 10 / 100) + 'S'
-delete phone.releaseYear
-console.log(phone);
+// phone.releaseYear = 2021
+// phone.price = 1099 + (1099 * 10 / 100) + 'S'
+// delete phone.releaseYear
+// console.log(phone);
+
+// Ex3
+let phones = [
+    {
+        phoneName: 'iPhone 13 Pro Max',
+        brand: [
+            {
+                brandName: 'Apple'
+            },
+            {
+                foundedYear: 1976
+            }
+        
+        ],
+        price: 30000000 + 'vnd',
+        country: 'Vietnam,USA,China',
+        phoneAvailable: 5000,
+    },
+    {
+        phoneName: 'Samsung S20',
+        brand: [
+            {
+                brandName: 'Samsung'
+            },
+            {
+                foundedYear: 1938
+            }
+        
+        ],
+        price: 12000000 + 'vnd',
+        country: 'Vietnam,USA,China',
+        phoneAvailable: 5000
+    }
+]
