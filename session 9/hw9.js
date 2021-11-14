a6067aafc6117629a51e825e7d1e1079e57c3811
Onissss <sonbui11113@gmail.com>
2021-11-14 10:59:57 +0700

    hw9

--- session 9/hw9.js
+++ session 9/hw9.js
@@ -12,15 +12,22 @@
 // myButton.style.width = '300px'
 // myButton.style.height = '300px'
 // let count = 0
-//     myButton.addEventListener('click' , () => {
-//         count += 1
-//         var colorR = Math.round(Math.random()*100)
-//         var colorG = Math.round(Math.random()*100)
-//         var colorB = Math.round(Math.random()*100)
-//         myButton.style.color = '#f7f7f7'
-//         myButton.innerText = `you clicked ${count}
-//         \n RGB(${colorR},${colorG},${colorB})`
+// myButton.addEventListener('click' , () => {
+//     count += 1
+//     var colorR = Math.round(Math.random()*255)
+//     var colorG = Math.round(Math.random()*255)
+//     var colorB = Math.round(Math.random()*255)
+//     myButton.style.color = '#f7f7f7'
+//     myButton.innerText = `you clicked ${count}
+//     \n RGB(${colorR},${colorG},${colorB})`
 
-//         myButton.style.backgroundColor = `rgb(${colorR},${colorG},${colorB})`
-//     })
+//     myButton.style.backgroundColor = `rgb(${colorR},${colorG},${colorB})`
+// })
 
+// Ex4
+// const myButton = document.getElementById('button')
+// console.log(myButton);
+// myButton.addEventListener('click' , () => {
+//     var rand = Math.round(Math.random() * 100 - 100)
+//     myButton.innerText = rand
+// })
